e3998a0f1bdbd822d95d6258df1112a5e099a255
amcm329 <amcm329@hotmail.com>
2025-01-05 22:05:56 -0600

    Add files via upload

--- src/tests/test_classifier.py
+++ src/tests/test_classifier.py
@@ -1,1 +1,281 @@
+"""
+Module: spamfilter
+Description:
+    A script that contains all functions to test functionalities
+    about NaiveBayes classifier.
 
+Additional Information:
+    - Version: 0.0.1
+    - Date: 2025-01-06
+
+Author:
+    Aaron Castillo <amc224@imperial.ac.uk>
+"""
+
+
+import unittest
+import pandas as pd
+#from classifier import NaiveBayes
+from spamfilter import document_terms, compute_word_counts, NaiveBayes
+
+
+class TestNaiveBayes(unittest.TestCase):
+    """
+    Unit tests for the NaiveBayes classifier.
+    
+    For these tests, we follow two approaches: 
+    1. ValueError - all the combinations that raise an error. 
+    2. Legit case - we use the right elements and expect a valid result.
+    """
+
+    def setUp(self):
+        """
+        Initialize test data and objects for the tests.
+        """
+        
+        #We define examples taken from the notes.
+        doc1 = ["call", "here", "win", "prize", "money"]
+        doc2 = ["call", "money", "call", "money", "bargain"]
+        doc3 = ["call", "here", "information"]
+        word_lists = [doc1, doc2, doc3]
+
+        #We create document_term_dataframe and word_counts
+        dtm = document_terms(word_lists)
+        self.spam_types = ["spam", "spam", "ham"]
+        self.word_counts = compute_word_counts(dtm, self.spam_types)
+        
+        #We initialize a NaiveBayes object.
+        self.nb = NaiveBayes(self.word_counts, self.spam_types)
+
+
+    def test_invalid_word_counts_type(self):
+        """
+        For NaiveBayes object: Test if ValueError is raised with the correct message when 
+        word_counts is not a DataFrame.
+        """
+        
+        with self.assertRaisesRegex(ValueError, "word_counts must be a pandas DataFrame."):
+            NaiveBayes([], self.spam_types)
+
+
+    def test_invalid_spam_types_type(self):
+        """
+        For NaiveBayes object: Test if ValueError is raised with the correct message when 
+        spam_types is not a valid list.
+        """
+        
+        with self.assertRaisesRegex(ValueError, r"spam_types must be a list of strings \('ham' or 'spam'\)."):
+            NaiveBayes(self.word_counts, "anything_but_a_list")
+
+
+    def test_empty_word_counts(self):
+        """
+        For NaiveBayes object: Test if ValueError is raised with the correct message when 
+        word_counts is empty.
+        """
+        
+        empty_df = pd.DataFrame()
+        
+        with self.assertRaisesRegex(ValueError, "word_counts must be non-empty."):
+            NaiveBayes(empty_df, self.spam_types)
+
+
+    def test_empty_spam_types(self):
+        """
+        For NaiveBayes object: Test if ValueError is raised with the correct message when 
+        spam_types is empty.
+        """
+        
+        with self.assertRaisesRegex(ValueError, "spam_types must be non-empty."):
+            NaiveBayes(self.word_counts, [])
+
+
+    def test_invalid_word_counts_index(self):
+        """
+        For NaiveBayes object: Test if ValueError is raised with the correct message when 
+        word_counts index is incorrect.
+        """
+        
+        #We create another word_counts and modify its index.
+        invalid_word_counts = self.word_counts.copy()
+        invalid_word_counts.index = ["n_jam", "n_charm"]
+        
+        with self.assertRaisesRegex(ValueError, r"word_counts index must be \[\"n_ham\", \"n_spam\"\]."):
+            NaiveBayes(invalid_word_counts, self.spam_types)
+
+
+    def test_invalid_alpha_in_fit(self):
+        """
+        For fit function: Test if ValueError is raised with the correct message when 
+        alpha is NOT in the range (0,1].
+        """
+        
+        with self.assertRaisesRegex(ValueError, r"alpha must be in the range \(0,1\]."):
+            self.nb.fit(alpha=-0.1)
+
+
+    def test_print_without_fit(self):
+        """
+        For print function: Test if ValueError is raised with the correct message when 
+        print function is called before fit function.
+        """
+        
+        with self.assertRaisesRegex(ValueError, "Please, execute fit function first."):
+            self.nb.print()
+
+
+    def test_classify_without_fit(self):
+        """
+        For classify function: Test if ValueError is raised with the correct message when 
+        classify function is called before fit function.
+        """
+        
+        with self.assertRaisesRegex(ValueError, "Please, execute fit function first."):
+            self.nb.classify("this is a message")
+
+
+    def test_invalid_confusion_matrix_inputs_1(self):
+        """
+        For compute_confusion_matrix function: Test if ValueError is raised with the correct 
+        message for invalid inputs (length mismatch).
+        """
+        with self.assertRaisesRegex(ValueError, "Both true_labels and predicted_labels must have the same length."):
+             self.nb.compute_confusion_matrix(["spam","spam", "ham"], ["spam", "ham"])
+
+
+    def test_invalid_confusion_matrix_inputs_2(self):
+        """
+        For compute_confusion_matrix function: Test if ValueError is raised with the correct 
+        message for invalid inputs (true_labels is empty).
+        """
+        with self.assertRaisesRegex(ValueError, "true_labels must be non-empty."):
+            self.nb.compute_confusion_matrix([], ["spam", "ham"])
+ 
+ 
+    def test_invalid_confusion_matrix_inputs_3(self):
+        """
+        For compute_confusion_matrix function: Test if ValueError is raised with the correct 
+        message for invalid inputs (predicted_labels is empty).
+        """
+        with self.assertRaisesRegex(ValueError, "predicted_labels must be non-empty."):
+            self.nb.compute_confusion_matrix(["spam", "ham"], [])
+
+
+    def test_invalid_confusion_matrix_inputs_4(self):
+        """
+        For compute_confusion_matrix function: Test if ValueError is raised with the correct 
+        message for invalid inputs (true_labels is not a valid structure).
+        """
+     
+        with self.assertRaisesRegex(ValueError, r"true_labels must be a list and contain only \"ham\" or \"spam\" labels."):
+             self.nb.compute_confusion_matrix(["invalid", "label"],["spam", "ham"])
+
+
+    def test_invalid_confusion_matrix_inputs_5(self):
+        """
+        For compute_confusion_matrix function: Test if ValueError is raised with the correct 
+        message for invalid inputs (predicted_labels is not a valid structure).
+        """
+        
+        with self.assertRaisesRegex(ValueError, r"predicted_labels must be a list and contain only \"ham\" or \"spam\" labels."):
+            self.nb.compute_confusion_matrix(["spam", "ham"], ["invalid", "label"])
+
+
+    def test_invalid_print_confusion_matrix_1(self):
+        """
+        For print_confusion_matrix function: Test if ValueError is raised with the correct 
+        message for invalid confusion matrix structure (one of the rows is all zeroes).
+        """
+        with self.assertRaisesRegex(ValueError, "All values in row 'ham' are zero."):
+            self.nb.print_confusion_matrix(matrix={"ham": {"spam": 0, "ham": 0}})
+
+
+    def test_invalid_print_confusion_matrix_2(self):
+        """
+        For compute_confusion_matrix function: Test if ValueError is raised with the correct 
+        message for invalid confusion matrix structure (one of the values is negative).
+        """
+        with self.assertRaisesRegex(ValueError, "Confusion matrix contains at least one negative value."):
+            self.nb.print_confusion_matrix(matrix={"ham": {"spam": 5, "ham": -1}})
+
+
+    def test_invalid_compute_accuracy_1(self):
+        """
+        For compute_accuracy function: Test if ValueError is raised with the correct 
+        message for invalid confusion matrix structure (matrix is not a dictionary).
+        """
+        with self.assertRaisesRegex(ValueError, "matrix must be a dictionary."):
+             self.nb.compute_accuracy([])
+        
+        
+    def test_invalid_compute_accuracy_2(self):
+        """
+        For compute_accuracy function: Test if ValueError is raised with the correct 
+        message for invalid confusion matrix structure (matrix is empty).
+        """
+        with self.assertRaisesRegex(ValueError, "matrix must be not empty."):
+             self.nb.compute_accuracy({})
+        
+       
+    def test_invalid_compute_accuracy_3(self):
+        """
+        For compute_accuracy function: Test if ValueError is raised with the correct 
+        message for invalid confusion matrix structure (one of the rows is all zeroes).
+        """
+        
+        with self.assertRaisesRegex(ValueError, "All values in row 'ham' are zero."):
+             self.nb.compute_accuracy(matrix={"ham": {"spam": 0, "ham": 0}})
+ 
+
+    def test_invalid_compute_accuracy_4(self):
+        """
+        For compute_accuracy function: Test if ValueError is raised with the correct 
+        message for invalid confusion matrix structure (one of the values is negative).
+        """
+        with self.assertRaisesRegex(ValueError, "Confusion matrix contains at least one negative value."):
+             self.nb.compute_accuracy(matrix={"ham": {"spam": 5, "ham": -1}})
+
+
+    def test_legit_case(self):
+        """
+        Test the classifier in a valid scenario by using the previous example data.
+        """
+        
+        #We fit the classifier.
+        self.nb.fit(alpha=1)
+
+        #We classify a test message (result is spam in this case).
+        test_message = "call money information"
+        result = self.nb.classify(test_message)
+        
+        #Check 1. Verify that classify returns a valid label. 
+        self.assertEqual(result, "spam")
+
+        #We create a customised confusion matrix and calculate its accuracy.
+        true_labels = ["spam", "spam", "ham"]
+        predicted_labels = [self.nb.classify(message) for message in ["call here", "money bargain", "information here"]]
+        confusion_matrix = self.nb.compute_confusion_matrix(true_labels, predicted_labels)
+        
+        #Accuracy is 1.
+        accuracy = self.nb.compute_accuracy(confusion_matrix)
+
+        #Check 2. We validate the confusion matrix structure.
+        self.assertEqual(set(confusion_matrix.keys()), {"ham", "spam"})
+        
+        #Check 3. We verify that the confusion matrix only contains ham and spam labels.
+        self.assertTrue(all(cls in confusion_matrix["ham"] for cls in ["ham", "spam"]))
+        self.assertTrue(all(cls in confusion_matrix["spam"] for cls in ["ham", "spam"]))
+
+        #Check 4. We check that all values in the matrix are non-negative.
+        self.assertEqual(all(value >= 0 for row in confusion_matrix.values() for value in row.values()), True)
+
+        #Check 5. We validate that no row has all its values as 0.
+        self.assertEqual(not any(all(value == 0 for value in row.values()) for row in confusion_matrix.values()),True)
+    
+        #Check 6. We validate that accuracy is 1.
+        self.assertEqual(accuracy, 1.0)
+
+
+# Run the tests
+#if __name__ == "__main__":
+#    unittest.main()
---
+++ src/tests/test_utils.py
@@ -0,0 +1,228 @@
+"""
+Module: spamfilter
+Description:
+    A script that contains all functions to test functionalities
+    implemented in util.py
+
+Additional Information:
+    - Version: 0.0.1
+    - Date: 2025-01-06
+
+Author:
+    Aaron Castillo <amc224@imperial.ac.uk>
+"""
+
+import unittest
+import pandas as pd
+from spamfilter import tokenize, document_terms, compute_word_counts
+
+
+class TestUtils(unittest.TestCase):
+    """
+    Unit tests for the utility script.
+    
+    For these tests, we follow two approaches: 
+    1. ValueError - all the combinations that raise an error. 
+    2. Legit case - we use the right elements and expect a valid result.
+    """
+
+    def test_tokenize_not_string(self):
+        """
+        For function tokenize: Test if raises ValueError when 
+        input is not a string.
+        """
+        
+        with self.assertRaisesRegex(ValueError, "Message must be a string."):
+            tokenize(123)
+
+
+    def test_tokenize_whitespace_string(self):
+        """
+        For function tokenize: Test if raises ValueError when 
+        input is a whitespace-only string.
+        """
+        
+        with self.assertRaisesRegex(ValueError, "The string is a whitespace. Please provide a valid string."):
+            tokenize("   ")
+
+
+    def test_tokenize_empty_string(self):
+        """
+        For function tokenize: Test if raises ValueError when 
+        input is an empty string.
+        """
+        
+        with self.assertRaisesRegex(ValueError, "The string is empty. Please provide a valid string."):
+            tokenize("")
+
+
+    def test_document_terms_not_list_of_lists(self):
+        """
+        For function document_terms: Test if raises ValueError when 
+        input is not a list of lists.
+        """
+        
+        with self.assertRaisesRegex(ValueError, "Input must be a list of word lists."):
+            document_terms("anything_but_a_list")
+
+
+    def test_document_terms_inner_elements_not_strings(self):
+        """
+        For function document_terms: Test if raises ValueError when 
+        inner elements are not strings.
+        """
+        
+        with self.assertRaisesRegex(ValueError, "All elements of the inner lists must be strings."):
+            document_terms([["token", 123]])
+
+
+    def test_document_terms_empty_input(self):
+        """
+        For function document_terms: Test if raises ValueError 
+        when input is empty.
+        """
+        
+        with self.assertRaisesRegex(ValueError, "Input is empty. Please provide a valid list."):
+            document_terms([])
+
+
+    def test_compute_word_counts_invalid_dataframe(self):
+        """
+        For function word_counts: Test if raises ValueError when 
+        document_term_dataframe is not a DataFrame.
+        """
+        with self.assertRaisesRegex(ValueError, "document_term_dataframe must be a pandas DataFrame."):
+            compute_word_counts("anything_but_a_dataframe", ["spam", "ham"])
+
+
+    def test_compute_word_counts_invalid_spam_types(self):
+        """
+        For function word_counts: Test if raises ValueError when 
+        spam_types is invalid.
+        """
+        
+        #We create a dummy dtm.
+        doc1 = ["call", "here"]
+        dtm = document_terms([doc1])
+        
+        with self.assertRaisesRegex(ValueError, r"spam_types must be a list of strings \('ham' or 'spam'\)."):
+            compute_word_counts(dtm, "anything_but_a_list")
+
+
+    def test_compute_word_counts_length_mismatch(self):
+        """
+        For function word_counts: Test if raises ValueError when 
+        length of spam_types does not match rows in document_term_dataframe.
+        """
+        
+        #We create a dummy dtm.
+        doc1 = ["call", "here"]
+        dtm = document_terms([doc1])
+        
+        with self.assertRaisesRegex(ValueError, "Length of spam_types must match the number of rows in document_term_dataframe."):
+            compute_word_counts(dtm, ["spam", "ham", "ham"])
+
+
+    def test_compute_word_counts_empty_dataframe(self):
+        """
+        For function word_counts: Test if raises ValueError when 
+        document_term_dataframe is empty.
+        """
+        
+        #We create an empty dataframe.
+        empty_df = pd.DataFrame()
+        
+        with self.assertRaisesRegex(ValueError, "document_term_dataframe must be non-empty."):
+            compute_word_counts(empty_df, ["spam", "ham"])
+
+
+    def test_compute_word_counts_empty_spam_types(self):
+        """
+        For function word_counts: Test if raises ValueError when 
+        spam_types is empty.
+        """
+        
+        #We create a dummy dtm.
+        doc1 = ["call", "here"]
+        dtm = document_terms([doc1])
+        
+        with self.assertRaisesRegex(ValueError, "spam_types must be non-empty."):
+            compute_word_counts(dtm, [])
+
+
+    def test_legit_case(self):
+        """
+        Test the functions with a valid scenario.
+        """
+
+        #Creating tests for tokenize function.    
+        message = "this is a test"
+        expected = ["this", "is", "a", "test"]
+        
+        #Check 1. Test for tokenize function.
+        self.assertEqual(tokenize(message), expected)      
+
+        #We create variables for document_term_dataframe function.    
+        word_lists = [["call", "here"], ["call", "money"]]
+        dtm_expected_columns = ["call", "here", "money"]
+        dtm = document_terms(word_lists)
+        
+        #dtm is:  
+        #    call  here  money
+        #0     1     1      0
+        #1     1     0      1        
+        
+        #Check 2. Check that the columns match the expected ones.
+        self.assertEqual(list(dtm.columns), dtm_expected_columns)
+        
+        #Check 3. Verify that the values of the first row coincide.
+        self.assertEqual(dtm.loc[0, "call"], 1)
+        self.assertEqual(dtm.loc[0, "here"], 1)       
+        self.assertEqual(dtm.loc[0, "money"], 0)
+        
+        #Check 3. Verify that the values of the second row coincide.
+        self.assertEqual(dtm.loc[1, "call"], 1)
+        self.assertEqual(dtm.loc[1, "here"], 0)       
+        self.assertEqual(dtm.loc[1, "money"], 1)
+        
+        #We consider the following data for the word_counts function.
+        doc1 = ["call", "here", "win", "prize", "money"]
+        doc2 = ["call", "money", "call", "money", "bargain"]
+        doc3 = ["call", "here", "information"]
+        word_lists = [doc1, doc2, doc3]
+        dtm = document_terms(word_lists)
+        spam_types = ["spam", "spam", "ham"]
+        word_counts = compute_word_counts(dtm, spam_types)
+        word_counts_expected_columns = ["call", "here", "win", "prize", "money", "bargain", "information"]
+        
+        #word_counts is: 
+        #        call  here  win  prize  money  bargain  information
+        #n_ham      1     1    0      0      0        0            1
+        #n_spam     3     1    1      1      3        1            0        
+        
+        #Check 4. Verify that the indexes of word_counts are ["n_ham", "n_spam"].
+        self.assertEqual(list(word_counts.index), ["n_ham", "n_spam"])
+       
+        #Check 5. Verify that the columns match the expected ones..      
+        self.assertEqual(list(word_counts.columns), word_counts_expected_columns)
+                
+        #Check 6. Make sure that all the values in word_counts match the expected ones.
+        self.assertEqual(word_counts.loc["n_ham", "call"], 1)
+        self.assertEqual(word_counts.loc["n_spam", "call"], 3)
+        self.assertEqual(word_counts.loc["n_ham", "here"], 1)
+        self.assertEqual(word_counts.loc["n_spam", "here"], 1)
+        self.assertEqual(word_counts.loc["n_ham", "win"], 0)
+        self.assertEqual(word_counts.loc["n_spam", "win"], 1)
+        self.assertEqual(word_counts.loc["n_ham", "prize"], 0)
+        self.assertEqual(word_counts.loc["n_spam", "prize"], 1)
+        self.assertEqual(word_counts.loc["n_ham", "money"], 0)
+        self.assertEqual(word_counts.loc["n_spam", "money"], 3)
+        self.assertEqual(word_counts.loc["n_ham", "bargain"], 0)
+        self.assertEqual(word_counts.loc["n_spam", "bargain"], 1)
+        self.assertEqual(word_counts.loc["n_ham", "information"], 1)
+        self.assertEqual(word_counts.loc["n_spam", "information"], 0)
+
+       
+# Run the tests
+#if __name__ == "__main__":
+#    unittest.main()
